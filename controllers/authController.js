5705aa6c184acceb5c255382179f907352e1eafc
Musta99 <mustafizurrahman1403099@gmail.com>
2025-09-24 20:22:30 +0600

    Authentication Module Implemented

---
+++ controllers/authController.js
@@ -0,0 +1,81 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+
+const signUp = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and Password are required",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(401).json({
+        message: "Email Already in use",
+      });
+    }
+
+    const user = new User({ email, password });
+    await user.save();
+
+    res.status(201).json({
+      message: "Account is created successfully",
+      data: {
+        id: user._id,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const logIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please provide Email and Passoword",
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "You dont have account with this Email",
+      });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid Password",
+      });
+    }
+
+    const generatedToken = jwt.sign(
+      {
+        data: user.email,
+      },
+      process.env.SECRET_KEY_TOKEN,
+      { expiresIn: 60 * 60 }
+    );
+
+    res.status(200).json({
+      message: "Successfully logged in",
+      data: {
+        id: user._id,
+        email: user.email,
+        token: generatedToken,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { signUp, logIn };
--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import cors from "cors";
 import express from "express";
+import authRoutes from "../routes/authRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// mount routes
+app.use("/api/routes", authRoutes);
 
 export default app;
