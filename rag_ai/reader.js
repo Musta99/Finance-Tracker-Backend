302492f1bfde23b5c7e04d8be13d9638c0fe4140
Musta99 <mustafizurrahman1403099@gmail.com>
2025-10-07 22:08:40 +0600

    Vector-Embedding-Supabase

---
+++ rag_ai/reader.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import path from "path";
+import { fileURLToPath } from "url";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { FakeEmbeddings } from "langchain/embeddings/fake";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
+  apiKey: process.env.GOOGLE_API_KEY,
+  model: "gemini-2.0-flash",
+});
+
+// const embeddings = new FakeEmbeddings();
+
+try {
+  const pdfPath = path.join(__dirname, "../data/finance_tracker.pdf");
+  const loader = new PDFLoader(pdfPath);
+  const docs = await loader.load();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 100,
+  });
+
+  const splittedDocs = await splitter.splitDocuments(docs);
+
+  // Saving embedding to supabase vector store
+  const supabaseClient = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_API_KEY
+  );
+
+  const vectorStore = await SupabaseVectorStore.fromDocuments(
+    splittedDocs,
+    embeddings,
+    {
+      client: supabaseClient,
+      tableName: "documents",
+    }
+  );
+} catch (err) {
+  console.log("Some error occured", err);
+}
